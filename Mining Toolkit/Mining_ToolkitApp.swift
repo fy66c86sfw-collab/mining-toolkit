1e67d8f5266981a82e86714ccb1b17ab76c8de23
Jory Dion <Jory.Dion@gmail.com>
2025-10-31 13:05:02 -0700

    Initial Commit

---
+++ Mining Toolkit/Mining_ToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mining_ToolkitApp.swift
+//  Mining Toolkit
+//
+//  Created by Jory on 2025-10-31.
+//
+
+import SwiftUI
+
+@main
+struct Mining_ToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
